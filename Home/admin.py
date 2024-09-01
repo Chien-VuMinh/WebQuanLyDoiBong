59154d1e0a4ed167d305debcda6f680da41d791c
HuyQuang224 <116049189+HuyQuang224@users.noreply.github.com>
2024-09-01 20:28:21 +0700

    Update admin.py

--- Home/admin.py
+++ Home/admin.py
@@ -4,5 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(CauThu)
 admin.site.register(Doi)
+admin.site.register(MuaGiai)
 admin.site.register(TranDau)
 admin.site.register(KetQua)
